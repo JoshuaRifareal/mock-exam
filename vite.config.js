f162253274396a2443b8507546fb7d3868500be8
joshuarifareal <joshrifareal123@gmail.com>
2026-07-07 17:52:21 +0800

    added icons

--- vite.config.js
+++ vite.config.js
@@ -9,8 +9,14 @@ export default defineConfig({
       registerType: 'autoUpdate',
       includeAssets: [
         'favicon.ico',
-        'icons/apple-touch-icon.png',
-        'icons/maskable-icon.png'
+        'favicon-16x16.png',
+        'favicon-32x32.png',
+        'favicon-48x48.png',
+        'apple-touch-icon.png',
+        'apple-touch-icon-152x152.png',
+        'apple-touch-icon-120x120.png',
+        'maskable-icon.png',
+        'browserconfig.xml'
       ],
       manifest: {
         name: 'Mock Exam',
@@ -23,6 +29,18 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
+            src: '/icons/icon-96x96.png',
+            sizes: '96x96',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: '/icons/icon-144x144.png',
+            sizes: '144x144',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
             src: '/icons/icon-192x192.png',
             sizes: '192x192',
             type: 'image/png',
@@ -31,13 +49,6 @@ export default defineConfig({
           {
             src: '/icons/icon-512x512.png',
             sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
-          },
-          // Add more if you have them
-          {
-            src: '/icons/icon-144x144.png',
-            sizes: '144x144',
             type: 'image/png',
             purpose: 'any maskable'
           }
